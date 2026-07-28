fe173560762ed88ab805a3300759461838a520cf
xenoverseguy11 <naamanabderahman@gmail.com>
2026-07-28 20:00:48 +0100

    a simple program that asks for a number, and outputs the double of it

--- doublethenumber.cpp
+++ doublethenumber.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-	std::cout << "Enter an integer: ";
-	int x;
-	std::cin >> x;
-	x = x * 2;
-	std::cout << "Double that number is: " << x << '\n';
+	std::cout << "Enter an integer: "; // prints the message
+	int x; // defining variable x
+	std::cin >> x; // receives value for x from the user
+	x = x * 2; // changes x's value to be multiplied by 2
+	std::cout << "Double that number is: " << x << '\n'; // prints the changed variable
 	return 0;
 }
