8149a8dc294bb075b0bfec899dd37c5f8e8cdf8f
xenoverseguy11 <naamanabderahman@gmail.com>
2026-07-28 19:11:37 +0100

    Update coutcinendl.cpp

--- coutcinendl.cpp
+++ coutcinendl.cpp
@@ -2,15 +2,33 @@
 
 int main()
 {
+#include <iostream>
+
+int main()
+{
     /*
 	THE IOSTREAM LIBRARY:
 	STD::COUT, STD::CIN, STD::ENDL
 
 	*/
-
+	int num1;
 	std::cout << "type a number" << std::endl; // std::cout : prints a text, std::endl : does a new line
+	std::cin >> num1; // std::cin : receives input from the user
+	std::cout << "the number you typed is " << num1 << std::endl; // uses std::cout to print the text and the modified variable "num1"
+	// and uses std::endl to make a new line
+
+	/*
+	SUMMARY
+	*/
+
+	// STD::COUT : outputs/prints a text,variable and more
+	// STD::CIN  : receives input from the user
+	// STD::ENDL : used to make a newline
 
 
+
+	return 0;
+}
 
 	return 0;
 }
