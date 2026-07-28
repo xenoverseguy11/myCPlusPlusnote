2c7ec25129aba8799f3618eb1859313a220fb994
xenoverseguy11 <naamanabderahman@gmail.com>
2026-07-29 00:47:03 +0100

    -- simple math

---
+++ Projects/BasicCalculator/math.cpp
@@ -0,0 +1,20 @@
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+int Sub(int x, int y)
+{
+	return x - y;
+}
+
+int Mult(int x, int y)
+{
+	return x * y;
+}
+
+int Div(int x, int y)
+{
+	return x / y;
+}
+
