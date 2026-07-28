105b0cf522f6e819ca66ca90f0feb196cae74b2a
xenoverseguy11 <naamanabderahman@gmail.com>
2026-07-28 19:08:53 +0100

    the iostream library: std::cout,cin,and endl

---
+++ coutcinendl.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    /*
+	THE IOSTREAM LIBRARY:
+	STD::COUT, STD::CIN, STD::ENDL
+
+	*/
+
+	std::cout << "type a number" << std::endl; // std::cout : prints a text, std::endl : does a new line
+
+
+
+	return 0;
+}
